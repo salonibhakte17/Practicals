f732ce25e33e3270589fd54ba8f3641c0047a106
Saloni Bhakte <salonibhakte@gmail.com>
2026-01-28 21:27:41 +0530

    Lab Assignment 1.1

---
+++ Lab1-1.py
@@ -0,0 +1,28 @@
+
+name = input("Enter employee name: ")
+emp_id = input("Enter employee ID: ")
+department = input("Enter department: ")
+basic_salary = float(input("Enter basic salary: "))
+
+
+da = 0.92 * basic_salary
+hra = 0.58 * basic_salary
+ta = 0.30 * basic_salary
+lic = 500
+
+
+gross_salary = basic_salary + da + hra + ta
+net_salary = gross_salary - lic
+
+
+print("\n----- Employee Salary Details -----")
+print("Name:", name)
+print("Employee ID:", emp_id)
+print("Department:", department)
+print("Basic Salary:", basic_salary)
+print("DA:", da)
+print("HRA:", hra)
+print("TA:", ta)
+print("LIC Deduction:", lic)
+print("Gross Salary:", gross_salary)
+print("Net Salary:", net_salary)
